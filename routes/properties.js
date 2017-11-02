94ca5406fc53fc4103739323d21d64adaed9a300
oliverpople <oliverpople@gmail.com>
2017-11-02 16:49:43 +0000

    Merging new routes and views

--- routes/properties.js
+++ routes/properties.js
@@ -5,6 +5,34 @@ var app = require('../app.js');
 var mongoUtil = require('../mongoUtil.js');
 var db = mongoUtil.getDb();
 
+
+app.get('/signup', function(req, res) {
+  res.render('signup.ejs');
+});
+
+
+app.post('/signup', function(req, res) {
+  db.collection('landlords').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+    console.log('saved to database');
+    res.redirect('/login');
+  });
+});
+
+app.get('/login', function(req, res) {
+  res.render('login.ejs');
+});
+
+
+app.post('/login', function(req, res) {
+  db.collection('landlords').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+    console.log('saved to database');
+    res.redirect('/listed-properties');
+  });
+});
+
+
 app.get('/listed-properties', function(req, res) {
   db.collection('properties').find().toArray(function(err, result) {
     if (err) return console.log(err);
---
+++ test/featureTest.js
@@ -0,0 +1,72 @@
+// force the test environment to 'test'
+process.env.NODE_ENV = 'test';
+var http = require('http');
+// get the application server module
+var app = require('../app');
+// use zombie.js as headless browser
+var Browser = require('zombie');
+var assert = require('assert');
+
+
+describe('makersbnb', function() {
+  before(function() {
+    this.server = http.createServer(app).listen(3000);
+    // initialize the browser using the same port as the test application
+    this.browser = new Browser({
+      site: 'http://localhost:3000'
+    });
+  });
+
+  describe('Sign up', function() {
+    before(function(done) {
+      this.browser.visit('/', done);
+    });
+
+    it('should sign up new user', function(done) {
+      this.browser.fill('emailAddress', 'email@example.com')
+      this.browser.fill('password', 'password69')
+      this.browser.fill('confirmPassword', 'password69')
+      this.browser.pressButton('Sign Up', done);
+    });
+
+
+  describe('Login', function() {
+    before(function(done) {
+      this.browser.visit('/login', done);
+    });
+
+    it('should log in a user', function(done) {
+      this.browser.fill('emailAddress', 'email@example.com')
+      this.browser.fill('password', 'password69')
+      this.browser.pressButton('Log in', done);
+    });
+  });
+
+  describe('post-property', function() {
+    before(function(done) {
+      this.browser.visit('/post-property', done);
+    });
+
+    it('should log in a user', function(done) {
+      this.browser.fill('property', 'big house')
+      this.browser.pressButton('Add property', done);
+    });
+  });
+
+  describe('view-properties', function() {
+    before(function(done) {
+      this.browser.visit('/view-properties', done);
+    });
+
+    it('shows user listed properties', function() {
+      assert.ok(this.browser.success);
+    });
+  });
+
+  });
+
+  after(function(done) {
+    this.server.close(done);
+  });
+
+});
